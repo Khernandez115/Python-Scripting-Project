4b8241d41e776ecdf76b40d6cc6f5868ba1a2a5c
khernandez13 <khernandez13@luc.edu>
2023-12-30 23:40:08 -0800

    Project Coding

--- get_game_data.py
+++ get_game_data.py
@@ -1,4 +1,4 @@
-import os
+import os 
 import json
 import shutil
 from subprocess import PIPE, run
@@ -6,9 +6,8 @@ import sys
 
 
 GAME_DIR_PATTERN = "game"
-GAME_CODE_EXTENSION = ".go"
+GAME_CODE_EXTENSTION = ".go"
 GAME_COMPILE_COMMAND = ["go", "build"]
-
 
 def find_all_game_paths(source):
     game_paths = []
@@ -18,59 +17,39 @@ def find_all_game_paths(source):
             if GAME_DIR_PATTERN in directory.lower():
                 path = os.path.join(source, directory)
                 game_paths.append(path)
-
         break
 
     return game_paths
-
-
-def get_name_from_paths(paths, to_strip):
-    new_names = []
-    for path in paths:
-        _, dir_name = os.path.split(path)
-        new_dir_name = dir_name.replace(to_strip, "")
-        new_names.append(new_dir_name)
-
-    return new_names
-
-
-def create_dir(path):
-    if not os.path.exists(path):
-        os.mkdir(path)
-
-
-def copy_and_overwrite(source, dest):
-    if os.path.exists(dest):
-        shutil.rmtree(dest)
-    shutil.copytree(source, dest)
-
 
 def make_json_metadata_file(path, game_dirs):
     data = {
         "gameNames": game_dirs,
         "numberOfGames": len(game_dirs)
     }
-
-    with open(path, "w") as f:
+    with open (path, "w") as f:
         json.dump(data, f)
+
+
+def copy_and_overwrite(source, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    shutil.copytree(source,dest)
 
 
 def compile_game_code(path):
     code_file_name = None
     for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith(GAME_CODE_EXTENSION):
+        for file in files: 
+            if file.endswith(GAME_CODE_EXTENSTION) :
                 code_file_name = file
                 break
-
-        break
+        break    
 
     if code_file_name is None:
         return
 
     command = GAME_COMPILE_COMMAND + [code_file_name]
     run_command(command, path)
-
 
 def run_command(command, path):
     cwd = os.getcwd()
@@ -88,9 +67,9 @@ def main(source, target):
 
     game_paths = find_all_game_paths(source_path)
     new_game_dirs = get_name_from_paths(game_paths, "_game")
-
+    
     create_dir(target_path)
-
+    
     for src, dest in zip(game_paths, new_game_dirs):
         dest_path = os.path.join(target_path, dest)
         copy_and_overwrite(src, dest_path)
@@ -100,10 +79,27 @@ def main(source, target):
     make_json_metadata_file(json_path, new_game_dirs)
 
 
+
+    
+
+def get_name_from_paths(paths, to_strip):
+    
+    new_names = []
+    for path in paths:
+        _, dir_name = os.path.split(path)
+        new_dir_name = dir_name.replace(to_strip, "")
+        new_names.append(new_dir_name)
+    return new_names
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
 if __name__ == "__main__":
     args = sys.argv
     if len(args) != 3:
-        raise Exception("You must pass a source and target directory - only.")
-
+        raise Exception("You must pass source and target directory only.")
+    
     source, target = args[1:]
     main(source, target)
+
